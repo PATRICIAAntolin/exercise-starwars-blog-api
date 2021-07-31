dccb4c9a795154a0db59c929379643edde2b802d
PATRICIAAntolin <prothings2021@gmail.com>
2021-07-31 20:33:45 +0000

    Cambios

--- src/main.py
+++ src/main.py
@@ -20,6 +20,8 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+current_user_id = 0
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +32,76 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def handle_users():
+    users = User.query.all()
+    users_all = []
+    for user in users:
+        users_all.append(users.serialize())
+    return jsonify(users_all), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/favorites', methods=['GET'])
+def handle_user_favorites():
+    favorites = Favorite.query.filter_by(user_id=current_user_id).all()
+    favorites_all = []
+    for favorite in favorites:
+        favorites_all.append(favorite.serialize())
+    return jsonify(favorites_all), 200
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['GET'])
+def handle_people():
+    characters = Character.query.all()
+    characters_all = []
+    for character in characters:
+        characters_all.append(character.serialize())
+    return jsonify(characters_all), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def handle_people_by_id(id):
+    character = Character.query.filter_by(id=id).first()
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    planets = Planet.query.all()
+    planets_all = []
+    for planet in planets:
+        planets_all.append(planet.serialize())
+    return jsonify(planets_all), 200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def handle_planets_by_id(id):
+    planet = Planet.query.filter_by(id=id).first()
+    return jsonify(planet.serialize()), 200
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST', 'DELETE'])
+def handle_favorite_planet_add_remove(planet_id):
+    if request.method == 'POST':
+        favorite = Favorite(userid = current_user_id, planet_id = planet_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'result': 'OK'})
+    else:
+        favorite = Favorites.query.filter_by(user_id=current_user_id, planet_id=planet_id).first()
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'result': 'OK'})
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST', 'DELETE'])
+def handle_favorite_people_add_remove(people_id):
+    if request.method == 'POST':
+        favorite = Favorite(userid = current_user_id, character_id = people_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'result': 'OK'})
+    else:
+        favorite = Favorites.query.filter_by(user_id=current_user_id, character_id=people_id).first()
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'result': 'OK'})
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
